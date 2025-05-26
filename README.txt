65a8e69d65431bbf3f4fa10df293e81b53666e8f
shidqimlna <shidqimlna@gmail.com>
2025-05-26 21:26:48 +0700

    first commit

---
+++ README.txt
@@ -0,0 +1,11 @@
+Credits:
+
+	Images:
+		Manypixels (manypixels.co)
+
+	Icons:
+		Font Awesome (fontawesome.io)
+
+	Other:
+		jQuery (jquery.com)
+		Scrollex (github.com/ajlkn/jquery.scrollex)
---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+(function ($) {
+    var $window = $(window),
+        $body = $('body'),
+        $header = $('#header'),
+        $banner = $('#banner');
+
+    // Breakpoints.
+    breakpoints({
+        xlarge: '(max-width: 1680px)',
+        large: '(max-width: 1280px)',
+        medium: '(max-width: 980px)',
+        small: '(max-width: 736px)',
+        xsmall: '(max-width: 480px)',
+    });
+
+    // Play initial animations on page load.
+    $window.on('load', function () {
+        window.setTimeout(function () {
+            $body.removeClass('is-preload');
+        }, 100);
+    });
+
+    // Header.
+    if ($banner.length > 0 && $header.hasClass('alt')) {
+        $window.on('resize', function () {
+            $window.trigger('scroll');
+        });
+
+        $banner.scrollex({
+            bottom: $header.outerHeight(),
+            terminate: function () {
+                $header.removeClass('alt');
+            },
+            enter: function () {
+                $header.addClass('alt');
+            },
+            leave: function () {
+                $header.removeClass('alt');
+            },
+        });
+    }
+})(jQuery);
+
+function goToByScroll(id) {
+    document
+        .getElementById(id)
+        .scrollIntoView({ block: 'start', behavior: 'smooth' });
+}
